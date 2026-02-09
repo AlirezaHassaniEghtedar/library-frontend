cce2045b6cccd5c2f5aed06025ffdfbd158f0c6a
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2026-02-10 02:58:15 +0330

    [UPDATE] wrap book item component with link

--- src/components/BookItem/BookItem.tsx
+++ src/components/BookItem/BookItem.tsx
@@ -14,20 +14,25 @@ type Props = {
 
 export default function BookItem({ book }: Props): ReactNode {
   return (
-    <li className={styles["book-item"]}>
-      {book.cover && (
-        <img src={book.cover} alt={`the picture of the book - ${book.title}`} />
-      )}
-      <div className={styles.data}>
-        <div className={styles.title}>Title : {book.title}</div>
-        <p>Description : {book.desc}</p>
-        <div>
-          <span>Price : {book.price}$</span>
-          <Link to={`/book/${book.id}`}>
-            more details <MingcuteRightFill />
-          </Link>
+    <Link to={`/book/${book.id}`}>
+      <li className={styles["book-item"]}>
+        {book.cover && (
+          <img
+            src={book.cover}
+            alt={`the picture of the book - ${book.title}`}
+          />
+        )}
+        <div className={styles.data}>
+          <div className={styles.title}>Title : {book.title}</div>
+          <p>Description : {book.desc}</p>
+          <div>
+            <span>Price : {book.price}$</span>
+            <span>
+              more details <MingcuteRightFill />
+            </span>
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 }
